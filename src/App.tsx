94e23356f0979abb1ae48dd84c5c1d3c18e36849
Denis <denis.junuzovic@fleetster.net>
2022-12-24 16:13:36 +0100

    setting up google maps

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from 'react';
+import { LoadScript, GoogleMap } from '@react-google-maps/api';
+import  mapStyles from './mapStyles.json';
+
+const containerStyle = {
+    width: '100%',
+    height: '500px'
+};
+
+const center = {
+    lat: 48.137154,
+    lng: 11.576124
+};
+
+const options = {
+    streetViewControl: false,
+    mapTypeControl: false,
+    panControl: false,
+    rotateControl: false,
+    fullscreenControl: false,
+    styles: mapStyles,
+    maxZoom: 20,
+    minZoom: 8
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [zoom, setZoom] = useState(options.minZoom);
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    return (
+        <div className="container pt-5">
+            <h1>CarSharing Map</h1>
+            <div className="col-12">
+                <LoadScript googleMapsApiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}>
+                    <GoogleMap
+                        mapContainerStyle={containerStyle}
+                        options={options}
+                        center={center}
+                        zoom={zoom}
+                    >
+                    </GoogleMap>
+                </LoadScript>
+            </div>
+        </div>
+    )
 }
 
 export default App
